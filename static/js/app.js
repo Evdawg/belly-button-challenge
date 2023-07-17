526e3088eb94091f45e656ea203d9f4836397b6e
Evan Sprecher <EvanSprecher7@gmail.com>
2023-07-17 18:55:43 -0500

    7/17/2023: Changed d3 from v7 to v5 to fix Uncaught error with .target function.

--- static/js/app.js
+++ static/js/app.js
@@ -4,25 +4,23 @@ const url = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/
 // const promise = d3.json(url);
 // console.log("Data Promise: ", promise)
 
-
 // let selectedItem = "940"; // #todo change this to be dynamic later on user selection of dropdown.
 // dropdown options will be from a list of the names array.
 
 let data = {};
 let selDataset = d3.select("#selDataset");
 
-
-
 // Fetch the JSON data and console log it to confirm imported:
 // split out the three arrays within the JSON data into separate variables:
 d3.json(url).then(
+    
     function(response) {
-    console.log(response);
-    data = response;
-    populateDropdown();
-    updateDashboard("940"); // #todo need to update this to be dynamic based on user dropdown selection
+        // console.log(response);
+        data = response;
+        populateDropdown();
+        updateDashboard(data.names[0]); // #todo need to update this to be dynamic based on user dropdown selection
     }
-);
+)
 
 // # todo: review after hours video and find out why this doesnt work. Throwing error. See 4hr time in video.
 function populateDropdown() {
@@ -36,41 +34,40 @@ function populateDropdown() {
         let newSelection = d3.event.target.value;
         updateDashboard(newSelection);
     });
-};
+}
 
 
 function updateDemographicInfo(metadata) {
-    let updateDemographicInfoDiv =  d3.select("#sample-metadata");
-    updateDemographicInfoDiv.html(
+    let demographicInfoDiv =  d3.select("#sample-metadata");
+    console.log(metadata);
+    demographicInfoDiv.html(
         `Ethnicity: ${metadata.ethnicity}<br>
-        Gender: ${metadata.gender}    
+         Gender: ${metadata.gender}    
         `
     );
-
-};
+}
 
 // #todo create a horizontal bar chart by setting x and y to values of uhhh... otu_ids, otu_labels and otu_values
-// from uhhh... the variabels containing arrays below. 
+// from uhhh... the variables containing arrays below. 
 function plotBarChart() {
 
 
-};
+}
 
 
 function plotBubbleChart() {
 
 
-};
+}
 
-// This function is to ensure that Dashboard is updated after API call is completed. I think...
+// This function is to ensure that the Dashboard is updated after API call is completed. I think...
 // #todo selectedItem will become a parameter to be passed in this function. Figure that out later.
 function updateDashboard(selectedItem) {
-    console.log(data)
+    console.log(data);
     let samples = data.samples;
-
     let arrayIndex = data.names.findIndex(val => val == selectedItem);
 
-    // dynamically 
+    // dynamically populate data variables
     let otu_ids = samples[arrayIndex].otu_ids;
     let otu_labels = samples[arrayIndex].otu_labels;
     let sample_values = samples[arrayIndex].sample_values;
@@ -85,16 +82,17 @@ function updateDashboard(selectedItem) {
         // Use sample_values as the values for the bar chart.
         // Use otu_ids as the labels for the bar chart.
         // Use otu_labels as the hovertext for the chart.
+    plotBarChart();
 
 
     // plot the gauge chart here.
-
+    // plotGauge();
 
 
 
     // plot the bubble chart here.
-
-};
+    plotBubbleChart();
+}
 
 
 
